721d8ca5aa421ebd53a7598f1237194ea263c718
Yeppman <yeppman13@gmail.com>
2021-01-28 22:21:05 +0100

    updated 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Home from './components/Home'
-import Home2 from './components/Home2'
 import MyList from './components/MyList'
 export default function App() {
   return (
     <Switch>
       <Route path="/"  exact component={Home}></Route>
-      <Route path="/home"  exact component={Home2}></Route>
       <Route path="/myList"  exact component={MyList}></Route>
     </Switch>
   )
